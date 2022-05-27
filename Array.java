856e03d0a38511a49c18ec75c7d40b019bd796d1
Roshan-7058 <106378226+Roshan-7058@users.noreply.github.com>
2022-05-27 19:23:56 +0530

    Update Array.java
    Code Captial Present

--- Array.java
+++ Array.java
@@ -22,7 +22,7 @@ public class Array {
 			int temp=array[i];
 			if(temp==num)
 			{
-				System.out.println("Value is present in ARRAY");
+				System.out.println("Value is PRESENT in ARRAY");
 				break;
 			}			
 					
